f5efe02001c2edd322f094519c40180ca77ed685
Nicolas de Campos Fischer <79231180+NicolasFischer2002@users.noreply.github.com>
2025-07-21 20:57:00 -0300

    Initial commit

---
+++ ClientConsoleApplication/Program.cs
@@ -0,0 +1,17 @@
+﻿
+try
+{
+	Console.ForegroundColor = ConsoleColor.Green;
+    Console.WriteLine("Aplicação iniciada");
+}
+catch (Exception ex)
+{
+    Console.ForegroundColor = ConsoleColor.Red;
+    Console.WriteLine($"\nErro inesperado: {ex.Message}");
+}
+finally
+{
+    Console.ForegroundColor = ConsoleColor.Blue;
+    Console.WriteLine($"\nAplicação finalizada");
+    Console.ForegroundColor = ConsoleColor.White;
+}
