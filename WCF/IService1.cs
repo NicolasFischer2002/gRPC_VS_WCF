3f1e2fa9ecee3850b3bf4639c94182c6ecd259ef
Nicolas de Campos Fischer <79231180+NicolasFischer2002@users.noreply.github.com>
2025-07-22 07:36:58 -0300

    Criado primeiro contrato no WCF para o benchmark

--- WCF/IService1.cs
+++ WCF/IService1.cs
@@ -1,47 +1,86 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Runtime.Serialization;
 using System.ServiceModel;
-using System.ServiceModel.Web;
-using System.Text;
 
 namespace WCF
-{
-    // OBSERVAÇÃO: Você pode usar o comando "Renomear" no menu "Refatorar" para alterar o nome da interface "IService1" no arquivo de código e configuração ao mesmo tempo.
+{    
     [ServiceContract]
     public interface IService1
     {
-
         [OperationContract]
-        string GetData(int value);
-
-        [OperationContract]
-        CompositeType GetDataUsingDataContract(CompositeType composite);
-
-        // TODO: Adicione suas operações de serviço aqui
+        ResolucaoBhaskara ResolverBhaskara(Bhaskara bhaskara);
     }
 
-
-    // Use um contrato de dados como ilustrado no exemplo abaixo para adicionar tipos compostos a operações de serviço.
     [DataContract]
-    public class CompositeType
+    public class ResolucaoBhaskara
     {
-        bool boolValue = true;
-        string stringValue = "Hello ";
-
         [DataMember]
-        public bool BoolValue
+        public bool ResolvidoComSucesso { get; private set; }
+        [DataMember]
+        public double[] Resolucao { get; private set; }
+        [DataMember]
+        public string MensagemErro { get; private set; }
+
+        public ResolucaoBhaskara(bool resolvidoComSucesso, double[] resolucao, string mensagemErro)
         {
-            get { return boolValue; }
-            set { boolValue = value; }
+            ResolvidoComSucesso = resolvidoComSucesso;
+            Resolucao = resolucao;
+            MensagemErro = mensagemErro;
+        }
+    }
+
+    [DataContract]
+    public class Bhaskara
+    {
+        [DataMember]
+        public double A { get; private set; }
+        [DataMember]
+        public double B { get; private set; }
+        [DataMember]
+        public double C { get; private set; }
+
+        public Bhaskara(double a, double b, double c)
+        {
+            ValidarBhaskara(a);
+
+            A = a;
+            B = b;
+            C = c;
         }
 
-        [DataMember]
-        public string StringValue
+        private void ValidarBhaskara(double a)
         {
-            get { return stringValue; }
-            set { stringValue = value; }
+            if (a == 0)
+                throw new ArgumentException("Coeficiente 'a' não pode ser zero em uma equação de 2º grau.");
+        }
+
+        private double CalcularDiscriminante()
+        {
+            return B * B - 4 * A * C;
+        }
+
+        public double[] Resolver()
+        {
+            double delta = CalcularDiscriminante();
+
+            if (delta < 0)
+            {
+                // Não há raízes reais
+                return Array.Empty<double>();
+            }
+
+            double sqrtDelta = Math.Sqrt(delta);
+            double x1 = (-B + sqrtDelta) / (2 * A);
+            double x2 = (-B - sqrtDelta) / (2 * A);
+
+            if (delta == 0)
+            {
+                // Raiz única (dupla)
+                return new[] { x1 };
+            }
+
+            // Duas raízes distintas
+            return new[] { x1, x2 };
         }
     }
 }
--- WCF/Service1.svc.cs
+++ WCF/Service1.svc.cs
@@ -1,33 +1,26 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Runtime.Serialization;
-using System.ServiceModel;
-using System.ServiceModel.Web;
-using System.Text;
+using System.Threading;
 
 namespace WCF
 {
-    // OBSERVAÇÃO: Você pode usar o comando "Renomear" no menu "Refatorar" para alterar o nome da classe "Service1" no arquivo de código, svc e configuração ao mesmo tempo.
-    // OBSERVAÇÃO: Para iniciar o cliente de teste do WCF para testar esse serviço, selecione Service1.svc ou Service1.svc.cs no Gerenciador de Soluções e inicie a depuração.
     public class Service1 : IService1
     {
-        public string GetData(int value)
+        public ResolucaoBhaskara ResolverBhaskara(Bhaskara bhaskara)
         {
-            return string.Format("You entered: {0}", value);
+			try
+			{
+                SimularLicenciamento();
+                return new ResolucaoBhaskara(true, bhaskara.Resolver(), string.Empty);
+            }
+			catch (Exception erro)
+			{
+                return new ResolucaoBhaskara(false, Array.Empty<double>(), $"{erro.Message}");
+			}
         }
 
-        public CompositeType GetDataUsingDataContract(CompositeType composite)
+        private void SimularLicenciamento()
         {
-            if (composite == null)
-            {
-                throw new ArgumentNullException("composite");
-            }
-            if (composite.BoolValue)
-            {
-                composite.StringValue += "Suffix";
-            }
-            return composite;
+            Thread.Sleep(50);
         }
     }
 }
